adbabeedf910411884738a07025cb1ce2edd0f5d
坂本のぞみ <sakamotonozomi@M-Nozomi-Sakamoto-noMacBookAir.local>
2017-02-12 17:24:16 +0900

    キーボード　修正

--- KyuntoApp/TableViewController/addViewController.swift
+++ KyuntoApp/TableViewController/addViewController.swift
@@ -143,7 +143,7 @@ class addViewController: UIViewController, UITextViewDelegate {
 
     
     //MARK: キーボードが出ている状態で、キーボード以外をタップしたらキーボードを閉じる
-   override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+   func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
         //非表示にする。
         if(addMemoTextView.isFirstResponder){
             addMemoTextView.resignFirstResponder()
